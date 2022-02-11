2600397cc1b20263afa9acfab6ad914dd4e3e42c
Pzeeuw-Cisco <pzeeuw@cisco.com>
2022-02-11 13:32:25 +0000

    Testing imports

--- Controller/Main.py
+++ Controller/Main.py
@@ -1,10 +1,14 @@
-
-
-
+import requests
+import json
+from SaseDashboard.Model.Thousand_Eyes_data import getToken as TE
 
 
 def main():
-    print("Hello World!")
+    url = "https://api.thousandeyes.com/v6/status"
+    username = "PhillyZ"
+    password = "RossyB"
+    token = TE(url, username, password)
+    print("token")
 
 if __name__ == "__main__":
     main()
---
+++ Model/Thousand_Eyes_data.py
@@ -0,0 +1,3 @@
+def getToken(url, username, password):
+    print("am testing if this {} is url and this {} is username and this {} is password".format(url, username, password))
+
