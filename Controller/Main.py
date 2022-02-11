3ca133b9803ebe361ae6dc256154c8c93c24c60f
Pzeeuw-Cisco <pzeeuw@cisco.com>
2022-02-11 16:28:26 +0000

    Imported view and model into controller main file

--- Controller/Main.py
+++ Controller/Main.py
@@ -1,13 +1,17 @@
 import requests
 import json
-from SaseDashboard.Model.Thousand_Eyes_data import getToken as TE
+import sys
+sys.path.insert(0, '/Users/pzeeuw/Documents/CSAP/VSCode Environment/SaseDashboard/Model')
+sys.path.insert(0, '/Users/pzeeuw/Documents/CSAP/VSCode Environment/SaseDashboard/View')
+import Thousand_Eyes_data, DUO_data, Umbrella_data
+import Grafana_data
 
 
 def main():
     url = "https://api.thousandeyes.com/v6/status"
     username = "PhillyZ"
     password = "RossyB"
-    token = TE(url, username, password)
+    token = Thousand_Eyes_data.getToken(url, username, password)
     print("token")
 
 if __name__ == "__main__":
